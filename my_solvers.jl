95382cb6b496096df3f4bd2e46b6c87c4eeb649d
Steven Walton <walton.stevenj@gmail.com>
2021-12-05 13:49:35 -0800

    CUDA implementation (testing)
    Seems the 3080 cannot run things properly so pushing to talapas

--- my_solvers.jl
+++ my_solvers.jl
@@ -1,10 +1,11 @@
 using LinearAlgebra
+using CUDA
 
 function conj_grad(A, x, b, epsilon, iter_max)
     r = b - A*x
     err = norm(r)
     if err < epsilon * norm(x)
-        @show "Exiting early"
+        #@show "Exiting too early"
         return x
     end
     p = r
@@ -17,8 +18,8 @@ function conj_grad(A, x, b, epsilon, iter_max)
         r = r - alpha * A * p
         err = norm(r) 
         if err < epsilon * norm(x)
-            @show "Exiting early step" 
-            @show k
+            #@show "Exiting early step" 
+            #@show k
             #return #x
             return x
         end
@@ -28,18 +29,55 @@ function conj_grad(A, x, b, epsilon, iter_max)
     return x
 end
 
-#let 
-#    N = 20
-#    B = rand(N, N)
-#    A = Matrix(I, N, N) + B'*B
-#    e = eigen(A)
-#
-#    b = rand(N);
-#    x = zeros(N);
-#    xexact = A\b # LU decomp
-#    #@show xexact
-#
-#    x = conj_grad(A, x, b, 1e-9, N^2)
-#    #@show x
-#    @show norm(x - xexact)
-#end
+function norm_cuda(x)
+    sum = CUDA.zeros(length(x))
+    for i = 1:length(x)
+        sum[i] = x[i] * x[i]
+    end
+    return sqrt(reduce(+, sum))
+end
+
+function conj_grad_cuda(A, x, b, epsilon, iter_max)
+    r = b - A * x
+    err = norm_cuda(r)
+    if err < epsilon * norm_cuda(x)
+        return x
+    end
+    p = copy(r)
+    for k = 1:iter_max
+        r_old = copy(r)
+        alpha = (r' * r) / (p' * A * p)
+        x .= x + alpha * p
+        err = norm_cuda(r)
+        if err < epsilon * norm_cuda(x)
+            return x
+        end
+        beta = (r' * r) / (r_old' * r_old)
+        p = r + beta * p
+    end
+    return x
+end
+
+let 
+    N = 20
+    B = rand(N, N)
+    A = Matrix(I, N, N) + B'*B
+    e = eigen(A)
+
+    b = rand(N);
+    x = zeros(N);
+    xexact = A\b # LU decomp
+    #@show xexact
+
+    #x = conj_grad(A, x, b, 1e-9, N^2)
+    A_cuda = CUDA.zeros(N, N)
+    for i = 1:N
+        A_cuda[i, i] = 2/0.01
+    end
+    x_cuda = CUDA.zeros(N)
+    b_cuda = CUDA.ones(N)
+    x = conj_grad_cuda(A_cuda, x_cuda, b_cuda, 1e-9, N^2)
+    #@show x
+    @show norm(x - xexact)
+end
+
--- poisson1D.jl
+++ poisson1D.jl
@@ -3,37 +3,129 @@ include("my_solvers.jl")
 using LinearAlgebra
 using SparseArrays
 using Plots
+using Printf
+using CUDA
 
-h = 0.1
-N = Integer(1/h + 1) # Total number of nodes
-m = N - 2 # total interior nodes
-
-x = 0:h:1
-A = zeros(m, m)
-for i = 1:m
-    A[i, i] = 2/h^2
+function get_error(solution, input, step)
+    u_total = [0; input; 0]
+    return sqrt((u_total - solution)' * (u_total - solution)) * sqrt(step)
 end
 
-for i = 1:m-1
-    A[i, i+1] = -1/h^2
-    A[i+1, i] = -1/h^2
+function poisson(h, eps)
+    N = Integer(1/h + 1) # Total number of nodes
+    m = N - 2 # total interior nodes
+    x0 = zeros(m)
+    x = 0:h:1
+    exact = sin.(pi*x)
+    A = zeros(m, m)
+    for i = 1:m
+        A[i, i] = 2/h^2
+    end
+
+    for i = 1:m-1
+        A[i, i+1] = -1/h^2
+        A[i+1, i] = -1/h^2
+    end
+
+    # make vector b
+    b = Array{Float64}(undef, m)
+    for i = 1:m
+        b[i] =  pi^2 * sin(pi * x[i]) 
+    end
+
+    u_int = conj_grad(A, x0, b, eps, N^2)
+    error = get_error(exact, u_int, h)
+
+    # Half
+    hd2 = h/2
+    N_half = Integer(1 / hd2 + 1)
+    m_half = N_half - 2
+    x0_half = zeros(m_half)
+    x_half = 0:hd2:1
+    exact_half = sin.(pi * x_half)
+    A_half = zeros(m_half, m_half)
+    for i = 1:m_half
+        A_half[i, i] = 2/hd2^2
+    end
+    for i = 1:m_half-1
+        A_half[i, i+1] = -1/hd2^2
+        A_half[i+1, i] = -1/hd2^2
+    end
+
+    # make vector b
+    b_half = Array{Float64}(undef, m_half)
+    for i = 1:m_half
+        b_half[i] =  pi^2 * sin(pi * x_half[i]) 
+    end
+
+    u_int_half = conj_grad(A_half, x0_half, b_half, eps, N_half^2)
+    error_half = get_error(exact_half, u_int_half, hd2)
+
+    @printf "%f\t %f\t %f\t %f\n" h error (error/error_half) log2(error)
 end
 
-# make vector b
-b = Array{Float64}(undef, m)
-for i = 1:m
-    b[i] =  pi^2 * sin(pi * x[i]) 
+function cuda_poisson(h, eps)
+    N = Integer(1/h + 1) # Total number of nodes
+    m = N - 2 # total interior nodes
+    x0 = CUDA.zeros(m)
+    x = CuArray{Float64}(0:h:1)
+    exact = sin.(pi* (0:h:1))
+    A = CUDA.zeros(m, m)
+    for i = 1:m
+        A[i, i] = 2/h^2
+    end
+
+    for i = 1:m-1
+        A[i, i+1] = -1/h^2
+        A[i+1, i] = -1/h^2
+    end
+
+    # make vector b
+    b = CuArray{Float64}(undef, m)
+    for i = 1:m
+        b[i] =  pi^2 * sin(pi * x[i]) 
+    end
+
+    u_int = conj_grad_cuda(A, x0, b, eps, N^2)
+    error = get_error(exact, u_int, h)
+
+    # Half
+    hd2 = h/2
+    N_half = Integer(1 / hd2 + 1)
+    m_half = N_half - 2
+    x0_half = CUDA.zeros(m_half)
+    x_half = CuArray{Float64}(0:hd2:1)
+    exact_half = sin.(pi * (0:hd2:1))
+    A_half = CUDA.zeros(m_half, m_half)
+    for i = 1:m_half
+        A_half[i, i] = 2/hd2^2
+    end
+    for i = 1:m_half-1
+        A_half[i, i+1] = -1/hd2^2
+        A_half[i+1, i] = -1/hd2^2
+    end
+
+    # make vector b
+    b_half = CuArray{Float64}(undef, m_half)
+    for i = 1:m_half
+        b_half[i] =  pi^2 * sin(pi * x_half[i]) 
+    end
+
+    u_int_half = conj_grad_cuda(A_half, x0_half, b_half, eps, N_half^2)
+    error_half = get_error(exact_half, u_int_half, hd2)
+
+    @printf "%f\t %f\t %f\t %f\n" h error (error/error_half) log2(error)
 end
-# form x0
-x0 = zeros(m);
 
-#(u_int, no_iters) = conj_grad(A, x0, b, 1e-6, N^2)
-u_int = conj_grad(A, x0, b, 1e-9, N^2)
-#@show u_int
+eps = 1e-9
+h_list = [0.1, 0.05, 0.025, 0.0125]
 
-exact = sin.(pi*x)
-u_total = [0;u_int;0] 
-#plot(x, u_total)
-#plot!(x, exact)
-error = sqrt((u_total - exact)'*(u_total - exact)) * sqrt(h)
-@show error
+@printf "dx\t\t error_dx\t ratio\t\t rate\n"
+#@time begin
+#    for i = 1:length(h_list)
+#        h = h_list[i]
+#        poisson(h, eps)
+#    end
+#end
+
+cuda_poisson(h_list[1], eps)
